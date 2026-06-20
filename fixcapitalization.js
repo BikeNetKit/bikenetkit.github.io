2ca4c1c25f50f731705c0e2d616c835dcde728d8
Michael Szell <michael@szell.net>
2026-06-20 21:37:05 +0200

    Revert "Outsource JS script due to github's CSP"
    This reverts commit 0b9be548d95171a02ca7e76bf99824df3ec5e9e6.

--- fixcapitalization.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    currentpath = window.location.pathname;
-    if (currentpath == "growbikenet") {
-        location.replace(window.location.hostname+"GrowBikeNet");
-    }
-};
